908c32e8c7f536bc4577ed4069708e2f0d3e6820
glafratta <g.lafratta@outlook.com>
2023-07-18 19:03:33 +0100

    started camerainterface

---
+++ cameraInterface.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "camera.h"
+
+typeDef int[3] pixel;
+
+class CameraInterface{
+enum Colour {RED GREEN BLUE};
+
+CameraInterface()= default;
+
+Colour whatColour()
+
+
+};
