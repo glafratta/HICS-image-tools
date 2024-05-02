e5ccf1ee1db81c205919cba68a3c0deed64fc13f
glafratta <g.lafratta@outlook.com~>
2024-05-02 11:53:30 +0100

    started debug

--- optic-flow.cpp
+++ optic-flow.cpp
@@ -1,14 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <math.h>
-#include "cameraInterface.h"
-#include <opencv2/highgui.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
+#include "imgProc.h"
 
-struct CameraCallback:Camera::SceneCallback{
-    CameraInterface cameraInterface;
+
+    // struct MyCallback : Libcam2OpenCV::Callback {
+	// Window* window = nullptr;
+	// virtual void hasFrame(const cv::Mat &frame, const libcamera::ControlList &) {
+	//     if (nullptr != window) {
+	// 	window->updateImage(frame);
+	//     }
+	// }
+    // };
+
+
+struct MyCallback : Libcam2OpenCV::Callback{
+    Window* window = nullptr;
 
     cv::Mat cropLeft(cv::Mat mat){
         return mat;
@@ -18,35 +22,42 @@ struct CameraCallback:Camera::SceneCallback{
         return mat;
     }
 
-    void nextScene(const cv::Mat & mat){
+    void hasFrame(const cv::Mat &frame, const libcamera::ControlList &){
         //featureparams (shi-tomasi corner detection) : these are fed to goodfeatures to track
-
         //lk params
 
         //crop frame x2: L/R
 
         //convert to grey
-        cv::Mat mat_grey;
-        cv::cvtColor(mat, mat_grey, cv::COLOR_BGR2GRAY);
-        cv::imshow("grey", mat_grey);
+        cv::Mat frame_grey;
+        cv::cvtColor(frame, frame_grey, cv::COLOR_BGR2GRAY);
+       // cv::imshow("grey", mat_grey);
 
         //goodfeatures to track
 
         //while(image is there)
             //crop and convert to grey
             //calcopticlalflowpyrLK
+        if (nullptr != window) {
+		window->updateImage(frame);
+	    }
     }
 };
 
 int main(){
-    int deviceID=0;
-    int apiID=0;
-    Camera camera;
-    CameraCallback cameraCallback;
-    camera.registerSceneCallback(&cameraCallback);
-    camera.start(deviceID, apiID);
-    do{
+    // int deviceID=0;
+    // int apiID=0;
+    // Camera camera;
+    // CameraCallback cameraCallback;
+    // camera.registerSceneCallback(&cameraCallback);
+    // camera.start(deviceID, apiID);
+    // do{
         
-    }while(!getchar());
-        camera.stop();
+    // }while(!getchar());
+    //     camera.stop();
+    QApplication app(argc, argv);
+
+        // create the window
+    Window window;
+    window.show();
 }
